1ca0c0fda06e11e8db0379f7282b36a46a737ea5
gevorg20077 <159165957+gevorg20077@users.noreply.github.com>
2024-04-13 19:55:50 +0400

    first commit

--- src/pages/PhotoPage/PhotoPage.jsx
+++ src/pages/PhotoPage/PhotoPage.jsx
@@ -1,21 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Calendar from '../../components/Calendar/Calendar';
-import { currentDateTime } from '../../store/slices/date/date';
+import { useSelector, useDispatch } from 'react-redux';
 import { openCalendar } from '../../store/slices/isCalendar/isCalendar';
-import { useDispatch, useSelector } from 'react-redux';
 import './photoPage.css';
 
 const PhotoPage = () => {
-  const dispatch = useDispatch()
-  dispatch(currentDateTime())
-  const changeLanguage = useSelector(state => state.changeLanguage)
-  const date = useSelector(state => state.date)
+  const dispatch = useDispatch();
+  const changeLanguage = useSelector(state => state.changeLanguage);
   const isCalendar = useSelector(state => state.isCalendar);
-  const calendarValue = useSelector(state => state.calendarValue)
-  console.log();
+  const calendarValue = useSelector(state => state.calendarValue);
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    if (calendarValue) {
+      setInputValue(`${calendarValue.day}/${calendarValue.month}/${calendarValue.year}`);
+    }
+  }, [calendarValue]);
+
   return (
     <form className="photoPage">
-      <input type="text" value={calendarValue.day + "/" + calendarValue.month + "/" + calendarValue.year} onFocus={() => dispatch(openCalendar())} />
+      <input type="text" value={inputValue} onClick={() => dispatch(openCalendar())} readOnly />
       {isCalendar && <Calendar />}
       <button>{changeLanguage.russian ? "Поиск" : changeLanguage.armenian ? "Փնտրել" : "Search"}</button>
     </form>
