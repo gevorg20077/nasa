016e51a678b7f8b755e5e69f03fa755d19cadad7
gevorg20077 <159165957+gevorg20077@users.noreply.github.com>
2024-04-13 18:58:51 +0400

    first commit

---
+++ src/pages/PhotoPage/PhotoPage.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import Calendar from '../../components/Calendar/Calendar';
+import { currentDateTime } from '../../store/slices/date/date';
+import { openCalendar } from '../../store/slices/isCalendar/isCalendar';
+import { useDispatch, useSelector } from 'react-redux';
+import './photoPage.css';
+
+const PhotoPage = () => {
+  const dispatch = useDispatch()
+  dispatch(currentDateTime())
+  const changeLanguage = useSelector(state => state.changeLanguage)
+  const date = useSelector(state => state.date)
+  const isCalendar = useSelector(state => state.isCalendar);
+  const calendarValue = useSelector(state => state.calendarValue)
+  // console.log();
+  return (
+    <form className="photoPage">
+      <input type="text" value={calendarValue.day + "/" + calendarValue.month + "/" + calendarValue.year} onFocus={() => dispatch(openCalendar())} />
+      {isCalendar && <Calendar />}
+      <button>{changeLanguage.russian ? "Поиск" : changeLanguage.armenian ? "Փնտրել" : "Search"}</button>
+    </form>
+  );
+};
+
+export default PhotoPage;
